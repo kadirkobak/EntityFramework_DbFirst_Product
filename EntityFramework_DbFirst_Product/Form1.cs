c75a3cd647be7cdb168338142571278160ab5e12
Kadir Kobak <kadir.kobak@celsan.com>
2025-03-27 14:58:48 +0300

    Category list operations updated.

--- EntityFramework_DbFirst_Product/Form1.cs
+++ EntityFramework_DbFirst_Product/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace EntityFramework_DbFirst_Product
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        EntityDbFirstEntities db = new EntityDbFirstEntities();
+
+        void CategoryList()
+        {
+            dataGridView1.DataSource = db.TblCategory.ToList();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            CategoryList();
         }
     }
 }
